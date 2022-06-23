1e96270ec8317bdf707593b226cde8f55ce29fec
NatzzXD <107767373+NatzzXD@users.noreply.github.com>
2022-06-23 14:22:22 +0700

    Update settings.js
    P

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "ᴹᴿ᭄DarkStalker BOT😈ོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ Luck Xxyོ ×፝֟͜×" //nama kalian
+global.botname = "Natnat-Isuki" //namabot kalian
+global.ownername= "NathanXD" //nama kalian
 global.myweb ="https://discord.gg/m4eRAs8Zf2" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UC5jaRLAdMkgxoLf8t9N37dw" //bebas asal jan hapus
 global.github = "GA ADA GITHUB OI" //bebas
 global.email = "ProDarkStalkerBOT@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6285904142544" // nomor wa kalian
-global.ownernomerr = "+6285904142544" //nmr wa kalian
+global.ownernomer = "6281563953229" // nomor wa kalian
+global.ownernomerr = "+6281563953229" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ['6285904142544','6283824566640','628895082547','62881027973711','6289517158630','6285848171414'] //ganti agar fitur owner bisa di gunakan
-global.packname = '© DarkStalker BOTོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Luck Xxy' //sticker wm ganti nama kalian
+global.packname = '© Natnat-Itsuki' //sticker wm ubah
+global.author = 'Di Buat Oleh Natnat' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
